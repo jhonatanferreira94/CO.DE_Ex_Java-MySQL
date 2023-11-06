9c40f173f6cfc7a647197be0b0891a3b37074abb
Jhonatan <jhonjhonpf@hotmail.com>
2023-11-06 19:17:38 -0300

    first commit

---
+++ src/main/java/Maneasy/Maneasy/ManeasyApplication.java
@@ -0,0 +1,13 @@
+package Maneasy.Maneasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManeasyApplication.class, args);
+	}
+
+}
